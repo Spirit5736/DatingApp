b827d1c2e5cfbad7c602ca937220eafd3f210a2c
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2021-09-21 15:32:42 +0700

    End of section 4

---
+++ API/Controllers/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Controllers.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration _config)
+        {
+             services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,28 +5,28 @@ using API.Entities;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+{ 
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
      public UsersController(DataContext context)
      {
          _context = context;
-     }   
-
+     }
+     
+     
      [HttpGet]
+     [AllowAnonymous]
      public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers()
      {
         return await _context.Users.ToListAsync();
          
      }
-
-    //api]users/3
-    [HttpGet ("{id}")]
+     [Authorize]
+     [HttpGet ("{id}")]
     public async Task<ActionResult<AppUser>>GetUser(int id)
      {
         return await _context.Users.FindAsync(id);
