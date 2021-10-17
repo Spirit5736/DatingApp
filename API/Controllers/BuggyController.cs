4295481140806d4bfa728b79824ba4ffeaa94dbe
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2021-10-17 15:38:29 +0700

    Added Error Handling

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -36,9 +36,16 @@ namespace API.Controllers
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
-            var thing = _context.Users.Find(-1);
-            var thingToReturn = thing.ToString();
-            return thingToReturn;
+            try
+            {
+                var thing = _context.Users.Find(-1);
+                var thingToReturn = thing.ToString();
+                return thingToReturn;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode (500, "Computer says no");
+            }            
         }
 
         [HttpGet("bad-request")]
