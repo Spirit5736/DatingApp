02f99dd7dde7368be4b076ab967a16bbd64b7423
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2021-10-20 22:16:41 +0700

    End of section 7

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -35,17 +35,10 @@ namespace API.Controllers
 
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
-        {
-            try
-            {
+        {          
                 var thing = _context.Users.Find(-1);
                 var thingToReturn = thing.ToString();
-                return thingToReturn;
-            }
-            catch (Exception ex)
-            {
-                return StatusCode (500, "Computer says no");
-            }            
+                return thingToReturn;            
         }
 
         [HttpGet("bad-request")]
