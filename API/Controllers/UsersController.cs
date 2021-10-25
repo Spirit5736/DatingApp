efe564bab23bcd17e6e058dddce789d77a84edab
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2021-10-25 23:34:00 +0700

    Added seeding data

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -19,13 +20,11 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
-        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
