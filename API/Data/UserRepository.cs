5574c456040623484a38b205182a2e875e4abb05
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2021-10-27 22:56:44 +0700

    End of section 8

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -11,34 +14,52 @@ namespace API.Data
     public class UserRepository : IUserRepository
     {
         private readonly DataContext _context;
+        private readonly IMapper _mapper;
 
-        public UserRepository(DataContext context)
+        public UserRepository(DataContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
-        public async Task<IEnumerable<AppUser>> GetUserAsync()
+
+        public async Task<MemberDto> GetMemberAsync(string username) //
+        {
+           return await _context.Users
+            .Where(x => x.UserName == username)
+            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+            .SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<MemberDto>> GetMembersAsync()//
+        {
+            return await _context.Users
+            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+            .ToListAsync();
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUserAsync()//
         {
             return await _context.Users.ToListAsync();
         }
 
-        public async Task<AppUser> GetUserByIdAsync(int id)
+        public async Task<AppUser> GetUserByIdAsync(int id)//
         {
            return await _context.Users.FindAsync(id);
         }
 
-        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        public async Task<AppUser> GetUserByUsernameAsync(string username)//
         {
             return await _context.Users
             .Include(p=>p.Photos)
             .SingleOrDefaultAsync(x=> x.UserName == username);
         }
 
-        public async Task<bool> SaveAllAsync()
+        public async Task<bool> SaveAllAsync()//
         {
             return await _context.SaveChangesAsync() > 0;
         }
 
-        public void Update(AppUser user)
+        public void Update(AppUser user)//
         {
             _context.Entry(user).State = EntityState.Modified;
         }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Controllers.Extensions;
 using API.DTOs;
 using API.Entities;
 using AutoMapper;
@@ -12,7 +13,10 @@ namespace API.Helpers
     {
       public AutoMapperProfiles()
       {
-         CreateMap<AppUser, MemberDto>();
+         CreateMap<AppUser, MemberDto>()
+         .ForMember(dest=>dest.PhotoUrl, opt => opt.MapFrom(src => 
+         src.Photos.FirstOrDefault(x=>x.IsMain).Url))
+         .ForMember(dest => dest.Age, opt => opt.MapFrom(src=>src.DateOfBirth.CalculateAge()));
          CreateMap<Photo, PhotoDto>();         
       }  
     }
