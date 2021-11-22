6aea319933e63fde2beb69a9f155b5469518d15d
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2021-11-22 17:11:40 +0700

    Added loading indicator

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -48,11 +50,13 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     FormsModule,
     ReactiveFormsModule,
     SharedModule,
+    NgxSpinnerModule
 
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
