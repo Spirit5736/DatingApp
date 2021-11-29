82c9d1721df670404db835ce588b872396f90ea3
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2021-11-29 23:20:50 +0700

    formatting

--- API/Controllers/Extensions/DateTimeExtensions.cs
+++ API/Controllers/Extensions/DateTimeExtensions.cs
@@ -7,13 +7,12 @@ namespace API.Controllers.Extensions
 {
     public static class DateTimeExtensions
     {
-      public static int CalculateAge(this DateTime dob)
-      {
-          var today = DateTime.Today;
-          var age = today.Year - dob.Year;
-          if(dob.Date > today.AddYears(-age)) 
-          age --;
-          return age;
-      }  
+        public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+            if (dob.Date > today.AddYears(-age)) age--;
+            return age;
+        }
     }
 }
