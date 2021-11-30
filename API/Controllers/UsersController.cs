0fd9da12c7ee072ad9396a8e8d7325127ad5a253
Ivan Keynes <ivan.keynes@bbconsult.co.uk>
2021-11-30 21:24:40 +0700

    Updated Create Route Method

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -38,7 +38,7 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
@@ -77,7 +77,10 @@ namespace API.Controllers
 
             user.Photos.Add(photo);
             if (await _userRepository.SaveAllAsync())
-            return _mapper.Map<PhotoDto>(photo);
+            {
+                return CreatedAtRoute("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+            }
+           
             return BadRequest ("Problem adding photo");
         }
     }
